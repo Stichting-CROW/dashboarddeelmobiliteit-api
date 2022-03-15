69b16d5ab90657da007a5b85b0db20978b7f7c48
bartwr <mail@bartroorda.nl>
2022-03-15 15:34:40 +0100

    Register no. of active users per day

---
+++ stats_active_users.py
@@ -0,0 +1,40 @@
+import hashlib
+
+def create_sha1_hash(str): 
+  # encode the string
+  encoded_str = str.encode()
+  # create a sha1 hash object initialized with the encoded string
+  hash_obj = hashlib.sha1(encoded_str)
+  # convert the hash object to a hexadecimal value
+  hexa_value = hash_obj.hexdigest()
+  # print
+  return hexa_value
+
+def register_active_user(conn, user):
+  if not conn:
+    return False
+
+  if not user.username:
+    return False
+
+  role = None
+  if user.is_admin:
+    role = 'admin'
+  else if user.filter_operator: 
+    role = 'operator'
+  else if user.filter.municipality:
+    role = 'municipality'
+
+  cur = conn.cursor()
+  stmt = """
+      INSERT INTO active_user_stats
+      (user_hash, role, active_on)
+      VALUES (%s, %s, NOW())
+      ON CONFLICT (active_user_on_date)
+      DO NOTHING;
+  """
+  try:
+      cur.execute(stmt, (create_sha1_hash(user.username), role))
+  except:
+      conn.rollback()
+  return True
